f256476a1c454fe4556d5d187e4ebde997d7a1ab
Kang-min Liu <gugod@gugod.org>
2014-10-23 21:01:09 +0200

    playing... dead simple key-value store.

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot name-of-things.core
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"]
+                 [ring-serve "0.1.2"]
+                 [ring/ring-jetty-adapter "1.3.0"]]
+  :plugins [[lein-ring "0.8.7"]]
+  :ring {:handler name-of-things.core/app}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/name_of_things/core.clj
+++ src/name_of_things/core.clj
@@ -1,7 +1,18 @@
 (ns name-of-things.core
-  (:gen-class))
+  (:use ring.middleware.params)
+  (:require [ring.util.response :as res]
+            [ring.util.request  :as req]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def mem (atom {}))
+
+(defn app [request]
+  (let [kv-k (req/path-info request)
+        kv-v (req/body-string request)]
+    (println (str kv-k " - " kv-v))
+    (case (:request-method request)
+      :post
+      (do
+       (swap! mem assoc kv-k kv-v)
+       (res/response ""))
+      :get
+      (res/response (@mem kv-k)))))
