d6bd5e3d2a24749543acfa38b6809496377ef1fc
Kang-min Liu <gugod@gugod.org>
2014-10-26 15:15:46 +0100

    reimplement using sqlite.

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
                  [ring/ring-core "1.3.0"]
                  [ring-serve "0.1.2"]
                  [ring/ring-jetty-adapter "1.3.0"]]
--- src/name_of_things/storage.clj
+++ src/name_of_things/storage.clj
@@ -1,10 +1,28 @@
-(ns name-of-things.storage)
+(ns name-of-things.storage
+  (:require [clojure.java.io   :as io]
+            [clojure.java.jdbc :as jdbc]))
 
-(def storage-root "/tmp/name-of-things")
+(def db
+  {:classname "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname "/tmp/name-of-things.db"
+   })
+
+(defn create-db []
+  (try (jdbc/db-do-commands
+        db
+        (jdbc/create-table-ddl "NameOfThings"
+                               [:name  :varchar "PRIMARY KEY"]
+                               [:thing :varchar "NOT NULL"]
+                               [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]))
+       (catch Exception e (println e))))
+
+(if (not (.exists (io/file "/tmp/name-of-things.db")))
+  (create-db))
 
 (defn get [k]
-  (slurp (str storage-root "/" k)))
+  (:thing (first (jdbc/query db ["select thing from NameOfThings where name = ?" k]))))
 
 (defn set [k v]
-  (spit (str storage-root "/" k) v))
+  (jdbc/insert! db "NameOfThings" {:name k :thing v}))
 
